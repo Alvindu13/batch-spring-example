f05f1449e4aee779ad6fac3f7cce75eb530ae3e8
Alvindu13 <alcaraz.jeremie@hotmail.fr>
2019-11-12 21:03:10 +0100

    Initial commit

---
+++ src/main/java/com/example/batchspringexample/BatchSpringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchspringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchSpringExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchSpringExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/batchspringexample/BatchSpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchspringexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchSpringExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
