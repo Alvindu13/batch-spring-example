fcdb26118f50ce8321befb63664735f0825ea255
Alvindu13 <alcaraz.jeremie@hotmail.fr>
2019-11-12 22:19:00 +0100

    added itemProcessor

--- src/main/java/com/example/batchspringexample/SpringBatchConfig.java
+++ src/main/java/com/example/batchspringexample/SpringBatchConfig.java
@@ -16,6 +16,7 @@ import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
@@ -48,6 +49,7 @@ public class SpringBatchConfig {
     }
 
 
+    @Bean
     public FlatFileItemReader<BankTransaction> flatFileItemReader(@Value("${inputFile}") Resource resource){
         FlatFileItemReader<BankTransaction> flatFileItemReader = new FlatFileItemReader<>();
         flatFileItemReader.setName("CSV-READER");
@@ -59,7 +61,8 @@ public class SpringBatchConfig {
 
     }
 
-    private LineMapper<BankTransaction> lineMapper() {
+    @Bean
+    public LineMapper<BankTransaction> lineMapper() {
         DefaultLineMapper<BankTransaction> lineMapper = new DefaultLineMapper<>();
         DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
         lineTokenizer.setDelimiter(",");
@@ -67,8 +70,10 @@ public class SpringBatchConfig {
         lineTokenizer.setNames("id", "accountID", "strDate", "transactionType", "amount");
         lineMapper.setLineTokenizer(lineTokenizer);
         BeanWrapperFieldSetMapper<BankTransaction> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
-        fieldSetMapper. setTargetType(BankTransaction.class);
+        fieldSetMapper.setTargetType(BankTransaction.class);
         lineMapper.setFieldSetMapper(fieldSetMapper);
         return lineMapper;
     }
+
+
 }
